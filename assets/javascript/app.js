3bb94915a92bcd2276b600b434b45bf098d5cabd
Jav <jvillegas427@gmail.com>
2019-06-29 14:57:39 -0700

    added more functions and giphy API functionality to javascript file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,15 @@ $(document).ready(function () {
     generateButtons();
 })
 
-var vgArray = ["halo", "pokemon", "for honor"];
+var vgArray = ["halo 3", "pokemon", "for honor"];
 
 function generateButtons() {
     $("#videogame-btns").empty()
     for (var i = 0; i < vgArray.length; i++) {
-        var buttonMaker = $("<button class='btn btn-primary'>");
+        var buttonMaker = $("<button class='btn btn-primary giphyShow'>")
+        buttonMaker.val(vgArray[i]);
+
+
         buttonMaker.text(vgArray[i]);
         $("#videogame-btns").append(buttonMaker);
     }
@@ -15,19 +18,7 @@ function generateButtons() {
 
 
 
-$.ajax({
-    url: "https://api.giphy.com/v1/gifs/search?q=videogames&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10",
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
 
-    var data = response.data;
-
-    for (var i = 0; i < data.length; i++) {
-        var newImg = $("<img class='gifz'>").attr("src", data[i].images.fixed_height.url)
-        $("#vg-gifs").append(newImg);
-    }
-})
 
 $("#subway").on("click", function (event) {
     event.preventDefault();
@@ -40,4 +31,21 @@ $("#subway").on("click", function (event) {
 
 
 
+$(document).on("click", ".giphyShow", function () {
+    var btnVal = $(this).val();
+    console.log(btnVal);
 
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q="+ btnVal +"&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10",
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        var data = response.data;
+
+        for (var i = 0; i < data.length; i++) {
+            var newImg = $("<img class='gifz'>").attr("src", data[i].images.fixed_height.url)
+            $("#vg-gifs").append(newImg);
+        }
+    })
+})

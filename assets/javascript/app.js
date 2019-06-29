705120b8ddc33734f4dfafd13367abc7a88e5a69
Jav <jvillegas427@gmail.com>
2019-06-29 12:22:22 -0700

    created base html css and js files, setup rows and columns in html

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+$.ajax({
+    url: "https://api.giphy.com/v1/gifs/search?q=videogames&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10",
+    method: "GET"
+}).then(function(response){
+console.log(response);
+
+
+
+
+})

984e45872c08d143b585ef663ac4b4285749c318
Jav <jvillegas427@gmail.com>
2019-06-29 14:27:50 -0700

    added variables and corresponding functions to javascript file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,43 @@
+$(document).ready(function () {
+    generateButtons();
+})
+
+var vgArray = ["halo", "pokemon", "for honor"];
+
+function generateButtons() {
+    $("#videogame-btns").empty()
+    for (var i = 0; i < vgArray.length; i++) {
+        var buttonMaker = $("<button class='btn btn-primary'>");
+        buttonMaker.text(vgArray[i]);
+        $("#videogame-btns").append(buttonMaker);
+    }
+}
+
+
+
 $.ajax({
     url: "https://api.giphy.com/v1/gifs/search?q=videogames&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10",
     method: "GET"
-}).then(function(response){
-console.log(response);
+}).then(function (response) {
+    console.log(response);
 
+    var data = response.data;
 
-
-
+    for (var i = 0; i < data.length; i++) {
+        var newImg = $("<img class='gifz'>").attr("src", data[i].images.fixed_height.url)
+        $("#vg-gifs").append(newImg);
+    }
 })
+
+$("#subway").on("click", function (event) {
+    event.preventDefault();
+    var dynamite = $("#inputGame").val().trim();
+    vgArray.push(dynamite);
+    generateButtons();
+
+
+});
+
+
+
+
